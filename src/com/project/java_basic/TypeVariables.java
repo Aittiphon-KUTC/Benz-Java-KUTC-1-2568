deb6c381f87e698a48a54f8a69aa9c359c123735
Aittiphon-KUTC <aittiphon.somsamer@gmail.com>
2025-06-13 11:58:20 +0700

    update file helloworld and variables

---
+++ src/com/project/java_basic/TypeVariables.java
@@ -0,0 +1,23 @@
+package com.project.java_basic;
+
+public class TypeVariables {
+    public static void main(String[] args) {
+        String name = "Benz"; // ประกาศตัวแปรแบบข้อความ
+//        System.out.println(name);
+
+        int number = 7;
+//        System.out.println(number);
+
+//        float total = (float)15.44;
+        double num = 15.44;
+        char A = 'b';
+        boolean result = false;
+        // Ture แปลว่า จริง
+        // False แปลว่า เท็จ
+
+        System.out.println(A+num+"Benz");
+
+
+
+    }
+}
